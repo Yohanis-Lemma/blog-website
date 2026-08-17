aa9af4891ea296ac26dd30615feae9a875bb74fb
Yohanis-Lemma <yohanislemma2017@gmail.com>
2026-08-17 18:35:12 +0300

    Add SEO sitemap and robots

---
+++ src/app/robots.ts
@@ -0,0 +1,11 @@
+import type { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://blog-website-khaki-five.vercel.app/sitemap.xml",
+  };
+}
